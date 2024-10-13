9c8eb52fcad12dd88ecd2c12726fb19f8796e077
Ruan Spinelli <114825105+RuanSpinelli@users.noreply.github.com>
2024-10-13 17:29:17 -0300

    SQL alchemy crud
    instalei o sqlalchemy no meu ambiente virtual e criei um crud

---
+++ api_atividade/models.py
@@ -0,0 +1,47 @@
+# Importando a função de criar um banco de dados do sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+
+# Importando algumas funções para fazer a sessão do banco de dados
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+
+from sqlalchemy.ext.declarative import declarative_base
+
+# Criando o banco de dados
+engine = create_engine("sqlite:///atividades.db")
+db_session = scoped_session(sessionmaker(autocommit=False, 
+                                         bind=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+class Pessoas(Base):
+    __tablename__ = "pessoa"
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40), index=True) 
+    idade = Column(Integer)
+
+    def __repr__(self):
+        return f'<Pessoa {self.nome}>'
+
+    def save(self):
+        db_session.add(self)
+        db_session.commit()
+
+
+    def delete(self):
+        db_session.delete(self)
+        db_session.commit()
+        
+
+class Atividades(Base):
+    __tablename__ = "atividades"  # Corrigido o nome da tabela
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(80))
+    pessoa_id = Column(Integer, ForeignKey('pessoa.id'))
+    pessoa = relationship("Pessoas")  # Corrigido para "Pessoas"
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    init_db()
---
+++ api_atividade/utils.py
@@ -0,0 +1,56 @@
+from models import Pessoas
+
+#inserir pessoas na tabela
+def insere_pessoas():
+    pessoa = Pessoas(nome= "Daniel", idade= 23) #cria uma instancia de uma pessoa usando os parametros da classe
+    #mostra a pessoa que foi inserida no banco de dados
+    #print(pessoa) 
+    
+    #adicionando a pessoa no banco de dados
+    pessoa.save()
+
+
+#consultar todas as pessoas que existem na tabela
+def consulta_pessoas():
+
+    print("Todas as pessoas que existem na tabela")
+    pessoa = Pessoas.query.all()
+    print(pessoa)
+    
+    #para retornar todos os usuários aonde o nome é daniel
+    #print("idade de daniel")
+    pessoa = Pessoas.query.filter_by(nome="Daniel").first()
+    #print(pessoa.idade) 
+    """
+    for i in pessoa:
+        print(i)
+    """
+
+
+#alterar as pessoas que existem na tabela
+def alterar_pessoa():
+    pessoa = Pessoas.query.filter_by(nome="Daniel").first()
+
+    #idade que quero que a pessoa tenha
+    pessoa.idade = 21
+    #fazer commit da alteração feita na pessoa
+    pessoa.save()
+
+#apagar pessoa da tabela pessoas
+def apagar_pessoa():
+    pessoa = Pessoas.query.filter_by(nome="Daniel").first()
+    pessoa.delete()
+
+
+
+
+#main
+if __name__ == "__main__":
+    #O crud
+    
+    
+    #insere_pessoas()
+    
+    #alterar_pessoa()
+    #apagar_pessoa()
+    consulta_pessoas()
